d622892e2a9a2303939686c82aedda2799c299f1
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-12 16:06:47 -0500

    Create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch('https://my-movies-rushdi.herokuapp.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            image: doc.ImageURL,
+            description: doc.Description,
+            genre: doc.Genre.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+  }
+
+  if (movies.length === 0) {
+    return <div>the list is empty</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
