bd49088c116e76be4a0744f57abeb01d14f694ea
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-12 16:06:38 -0500

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+export const MovieView = ({ movie, onBackClick }) => {
+    return (
+        <div>
+            <div>
+                <img src={movie.image} />
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Genre:</span>
+                <span>{movie.genre}</span>
+            </div>
+            <div>
+                <span>Description:</span>
+                <span>{movie.description}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
+
+        </div>
+    );
+};
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string,
+        genre: PropTypes.string,
+        description: PropTypes.string
+
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+};
