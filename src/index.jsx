baf74fa095dda52048d2298dc4383119b3289490
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-12 16:06:30 -0500

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
+
+const App = () => {
+  return (
+  
+      <MainView />
+  
+  );
+};
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App />);
