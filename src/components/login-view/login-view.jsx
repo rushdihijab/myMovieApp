929e401ca52055cddbc2ddadf7a6d378adff6eb1
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-17 14:40:07 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
@@ -33,26 +35,27 @@ export const LoginView = ({ onLoggedIn }) => {
             });
     }
     return (
-        <form onSubmit={handlSubmit}>
-            <label>
-                Username:
-                <input
+        <Form onSubmit={handlSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
                     type="text"
                     value={username}
-                    onChange={(e) => setUsername(e.target.value)} required
+                    onChange={(e) => setUsername(e.target.value)} required minLength="3"
                 />
-            </label>
-            <label>
-                Password:
-                <input
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
                     type="password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)} required
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                 />
-            </label>
-            <button type="submit">
+            </Form.Group>
+            <Button variant="primary" type="submit">
                 Submit
-            </button>
-        </form>
+            </Button>
+        </Form>
     );
 };
