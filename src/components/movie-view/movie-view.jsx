bccc9e382420c304cdd76b5699eced2de7bdbdc1
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-17 14:39:57 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,33 +1,47 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-export const MovieView = ({ movie, onBackClick }) => {
+
+
+export const MovieView = ({ movies }) => {
+
+    const { movieId } = useParams();
+
+    const movie = movies.find((m) => m.id === movieId);
     return (
-        <div>
-            <div>
-                <img src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Genre:</span>
-                <span>{movie.genre}</span>
-            </div>
-            <div>
-                <span>Description:</span>
-                <span>{movie.description}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
+        <>
+            <Card>
+                <div>
+                    <img className="w-100" src={movie.image} />
+                </div>
+                <div>
+                    <span>Title: </span>
+                    <span>{movie.title}</span>
+                </div>
+                <div>
+                    <span>Genre:</span>
+                    <span>{movie.genre}</span>
+                </div>
+                <div>
+                    <span>Description:</span>
+                    <span>{movie.description}</span>
+                </div>
+                <Link to={`/`}>
+                    <Button className="back-button">Back</Button>
+                </Link>
+            </Card>
+        </>
     );
+
 };
 MovieView.propTypes = {
-    movie: PropTypes.shape({
+    movies: PropTypes.shape({
         title: PropTypes.string,
         genre: PropTypes.string,
         description: PropTypes.string
 
     }).isRequired,
-    onBackClick: PropTypes.func.isRequired
+
 };
