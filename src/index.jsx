83a3b3915298a723bc5ff47e958cb3023b97b99e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-17 14:39:46 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const App = () => {
   return (
-
-    <MainView />
-
+    <Container fluid className="justify-content-md-center">
+      <MainView />
+    </Container>
   );
-};
+}
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
