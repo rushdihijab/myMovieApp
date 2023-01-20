84ff95a978aa2a95c43d3ed130453a93aa635680
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-20 14:54:01 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,6 +2,9 @@ import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row";
+import Col from 'react-bootstrap/Col';
 import "./movie-view.scss"
 
 
@@ -10,38 +13,131 @@ export const MovieView = ({ movies }) => {
     const { movieId } = useParams();
 
     const movie = movies.find((m) => m.id === movieId);
-    return (
-        <>
-            <Card className="movieview">
-                <div>
-                    <img className="w-100" src={movie.image} />
-                </div>
-                <div>
-                    <span>Title: </span>
-                    <span>{movie.title}</span>
-                </div>
-                <div>
-                    <span>Genre:</span>
-                    <span>{movie.genre}</span>
-                </div>
-                <div>
-                    <span>Description:</span>
-                    <span>{movie.description}</span>
-                </div>
-                <Link to={`/`}>
-                    <Button className="back-button">Back</Button>
-                </Link>
-            </Card>
-        </>
-    );
 
+
+    // const handleFavorite = () => {
+    //     fetch("https://my-movies-rushdi.herokuapp.com/users/" + user.Username + "/movies/" + movie._id, {
+    //         method: "POST",
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //             "Content-Type": "application/json"
+    //         }
+    //     }).then((response) => {
+    //         alert("Added to favorites!");
+    //         return response.json();
+    //     }).then(data => updateUser(data))
+    //         .catch(err => {
+    //             alert("Something went wrong");
+    //         });
+    // };
+
+    // const handleRemoveFavorite = () => {
+    //     fetch("https://my-movies-rushdi.herokuapp.com/users/" + user.Username + "/movies/" + movie._id, {
+    //         method: "DELETE",
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //             "Content-Type": "application/json"
+    //         }
+    //     }).then((response) => {
+    //         if (response.ok) {
+    //             alert("Removed from favorites");
+
+    //             updateUser(newUser);
+    //         } else {
+    //             alert("Something went wrong");
+    //         }
+    //     });
+    // };
+
+
+    return (
+
+        <Container className="cardset; content">
+            <Row>
+                <Card bg="dark" text="light">
+                    <Card.Header>
+                        <div className="title text-center">
+                            <span> {movie.title} </span>
+                        </div>
+                    </Card.Header>
+                    <Card.Body>
+                        <div>
+                            <div>
+                                <Card.Img
+                                    className="cardimage"
+                                    crossOrigin="anonymous"
+                                    src={movie.image}
+                                />
+                            </div>
+                            <div>
+                                <span className="labeltitle">Description: </span>
+                                <span className="description">{movie.description}</span>
+                            </div>
+                            <div>
+                                <span className="labeltitle">Release Year: </span>
+                                <span className="description">{movie.releaseYear}</span>
+                            </div>
+                            <div>
+                                <span className="labeltitle">Genre: </span>
+                                <span className="description">{movie.genre.name}</span>
+                                <span className="labeltitle"> Description:</span>
+                                <span className="description">{movie.genre.description}</span>
+                            </div>
+                            <div>
+                                <span className="labeltitle">Director: </span>
+                                <span className="description">{movie.director.name}</span>
+                                <div>
+                                    <span className="labeltitle">Bio </span>
+                                    <span className="description">{movie.director.bio}</span>
+                                    <span className="labeltitle">Birht </span>
+                                    <span className="description">{movie.director.birth}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Link to="/">
+                            <Button className="btn-login"> Back </Button>
+                        </Link>
+                        {/* {
+                            storedUser.FavoriteMovies.indexOf(movie._id) >= 0 ? (
+                                <Button
+                                    variant="danger"
+                                    onClick={() => handleRemoveFavorite(movie._id, "add")}
+                                >
+                                    Remove from Favorites
+                                </Button>
+                            ) : (
+                                <Button
+                                    className="button-add-favorite"
+                                    onClick={() => handleFavorite(movie._id, "add")}
+                                >
+                                    + Add to Favorites
+                                </Button>
+                            )
+                        } */}
+                    </Card.Footer>
+                </Card>
+            </Row>
+        </Container>
+    );
 };
+
 MovieView.propTypes = {
     movies: PropTypes.shape({
-        title: PropTypes.string,
-        genre: PropTypes.string,
-        description: PropTypes.string
-
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+        }),
+        Genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        }),
+        Director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bie: PropTypes.string.isRequired,
+            birth: PropTypes.string.isRequired,
+        }),
     }).isRequired,
-
 };
