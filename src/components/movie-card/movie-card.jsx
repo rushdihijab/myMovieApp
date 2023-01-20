88cb1bbb80aa49a1f71164e240bb4becc885ca13
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-20 14:54:10 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
+
 export const MovieCard = ({ movie }) => {
+
     return (
-        <div className="content">
-            <Card className="h-80; moviecardview" bg="dark" text="light">
-                <Card.Img className="imgcard" variant="top" crossOrigin="anonymous" src={movie.image} />
-                <Card.Body>
-                    <Card.Title className="title"> {movie.title} </Card.Title>
-                    <Card.Text className="description">
-                        Director Name: {movie.director.name}
-                    </Card.Text>
-                    <Card.Text className="genrecard"> {movie.genre.name} </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-                        <Button className="btn-login">Open</Button>
-                    </Link>
-                </Card.Footer>
-            </Card>
-        </div>
+        <Container className="content">
+            <Col>
+                <Card className="h-100; moviecardview" bg="dark" text="light">
+                    <Card.Img className="imgview" variant="top" crossOrigin="anonymous" src={movie.image} />
+                    <Card.Body>
+                        <Card.Title className="title"> {movie.title} </Card.Title>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+                            <Button className="btn-login">Open</Button>
+                        </Link>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </Container>
     );
 };
-
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
-        genre: PropTypes.string.isRequired,
+        id: PropTypes.isRequired,
+
     }).isRequired,
 };
