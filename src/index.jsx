df7c85127e7357b995024ea2f07fbca7993677a2
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-01-20 14:53:23 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,7 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const App = () => {
   return (
